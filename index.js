bd0583f52cbe7997f0e7dc1d0361032e9f5e6957
Stone Pack <stone.pack@outlook.com>
2020-11-05 11:18:22 -0800

    indexed API class

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+export { default as API } from './system/utilities/api';
+
 export { default as File } from './system/utilities/file';
 
-export { default as Test } from './system/test';
+export { default as Test } from './system/automation/test';
