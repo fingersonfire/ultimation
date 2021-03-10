5ac0fbcb665b6a9d58e28c0a540244f2aa656fb9
Stone Pack <stone.pack@outlook.com>
2021-03-09 18:37:38 -0800

    added environment configuration and data to index

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
+import Environment from 'dotenv';
+Environment.config();
+
 export { default as API } from './system/utilities/api';
+
+export { default as DATA } from './data/storage.json'
 
 export { default as File } from './system/utilities/file';
 
