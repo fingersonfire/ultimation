d3ce767bda6b2259aa0e459a4522b5dd451db60a
Stone Pack <stone.pack@outlook.com>
2021-03-09 20:15:53 -0800

    removed test library from index

--- index.js
+++ index.js
@@ -6,5 +6,3 @@ export { default as API } from './system/utilities/api';
 export { default as DATA } from './data/storage.json'
 
 export { default as File } from './system/utilities/file';
-
-export { default as Test } from './system/automation/test';
