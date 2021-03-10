20bec2897db32e5fbdf7f1d5a2ad0f08985de962
Stone Pack <stone.pack@outlook.com>
2021-03-09 20:54:48 -0800

    finished basic file class funcitonality

--- system/utilities/file.js
+++ system/utilities/file.js
@@ -4,20 +4,38 @@ export default class File {
 
     /**
      * Creates a file for the given path and file name
-     * @param {string} path - Relative path and file name
+     * @param {string} path - Relative path (starts from project root) including the file name
      */
     static create(path) {
-        // TODO: remove the last element in the path and write a blank file
         if(!FS.existsSync(path)) {
-            FS.mkdirSync(path, {recursive: true});
+            // Get the directory path
+            dir = path.split('/');
+            dir.pop();
+            dir = dir.toString().replace(',','/');
+
+            FS.mkdirSync(dir, {recursive: true});
+            FS.writeFileSync(path, '', (err) => {
+                if(err) {
+                    throw new Error(err);
+                }
+            });
         }
     }
 
-    static write(path, data) {
-        if(typeof(data) == 'object') {
-            data = JSON.stringify(data);
+    /**
+     * Write data to a JSON file
+     * @param {string} path - Relative path (starts from project root) including the file name
+     * @param {function} callback - Callback to manipulate data, must return final object
+     */
+    static write(path, callback) {
+        let data = this.read(path);
+        const returned = callback(data);
+
+        if(returned === undefined) {
+            throw new Error('The File.write funciton callback needs to return data');
         }
 
+        data = JSON.stringify(data);
         FS.writeFile(path, data, (err) => {
             if(err) {
                 throw new Error(err);
@@ -25,8 +43,13 @@ export default class File {
         });
     }
 
-    static read() {
-        
+    /**
+     * Read the contents of a JSON file
+     * @param {string} path - Relative path (starts from project root) including the file name
+     * @returns {object} - JS readable JSON object
+     */
+    static read(path) {
+        return JSON.parse(FS.readFileSync(path));
     }
 
 }
