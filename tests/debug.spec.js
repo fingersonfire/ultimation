4a76559a3489decdb622779b5d7f389f04096270
Stone Pack <stone.pack@outlook.com>
2021-03-09 20:18:43 -0800

    removed code from debug test

--- tests/debug.spec.js
+++ tests/debug.spec.js
@@ -1,8 +1,7 @@
-import { API, DATA, File, Test } from '../index';
+import { API, DATA, File } from '../index';
 
 async function main() {
-    const response = await API.get(`${DATA.endpoints.meh}${process.env.MEH_AUTH}`);
-    console.log(response.data.title);
+    
 }
 
 main();
