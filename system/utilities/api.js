fb0c8eafe3f4cfd6b8bcea29b32ca2b84fa05271
Stone Pack <stone.pack@outlook.com>
2021-03-09 20:18:16 -0800

    added formatter and post function to API class

--- system/utilities/api.js
+++ system/utilities/api.js
@@ -11,12 +11,30 @@ class API {
             }
         );
 
-        return {
-            status: resp.status,
-            data: resp.data
-        }
+        return _format(resp);
     }
 
+    async post(url, headers, json) {
+        const resp = await Axios(
+            {
+                method: 'post',
+                url: url,
+                headers: headers,
+                data: json
+            }
+        );
+
+        return _format(resp);
+    }
+
+}
+
+function _format(response) {
+    return {
+        data: response.data,
+        headers: response.headers,
+        status: response.status
+    }
 }
 
 export default new API
