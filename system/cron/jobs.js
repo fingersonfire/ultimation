9229bc6ffac0975abcd28b8539b5eee2b6cfb16c
Stone Pack <stone.pack@outlook.com>
2021-03-09 20:17:18 -0800

    finished creating meh notifier job

--- system/cron/jobs.js
+++ system/cron/jobs.js
@@ -1,9 +1,21 @@
+import { API, DATA } from '../../index';
 import Cron from 'cron';
 
 export const mehNotifier = new Cron.CronJob(
     '00 00 07 * * *',
-    () => {
-        console.log('The time is 9:19 PM');
+    async () => {
+        const productInfo = await API.get(`${DATA.endpoints.meh}${process.env.MEH_AUTH}`);
+
+        await API.post(
+            `${process.env.DISCORD_MEH_HOOK}`,
+            {
+                'Content-type' : 'application/json'
+            },
+            {
+                username: 'MehBot',
+                content: `${productInfo.data.deal.title}`
+            }
+        );
     },
     null,
     true,
