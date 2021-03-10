23d49ee5940015a982e251ea3d16a914b91833fb
Stone Pack <stone.pack@outlook.com>
2021-03-09 18:41:25 -0800

    created initial wrapper around axios

--- system/utilities/api.js
+++ system/utilities/api.js
@@ -1,55 +1,21 @@
-import fetch from 'node-fetch';
+import Axios from 'axios';
 
 class API {
 
-    get(url, headers = {}) {
-        let response;
-        _get(url, headers).then(result => console.log(result));
-        return response;
-    }
-
-    /**
-     * Make a request to an API endpoint
-     * @param {('GET'||'POST'||'DELETE'||'PUT')} method - API request method
-     * @param {string} url - URL endpoint to send request
-     * @param {object=} header 
-     * @param {object=} body 
-     */
-    request(method, url, headers = {'Content-Type': 'application/json'}, body = {}) {
-        fetch(
-            url, 
+    async get(url) {
+        const resp = await Axios(
             {
-                method: method,
-                headers: headers,
+                method: 'get',
+                url: url
             }
-        )
-        .then((res) => {
-            return res;
-        });
-    }
+        );
 
-}
-
-async function _get(url, headers) {
-    let response = {statusCode: '', body: ''};
-
-    const res = await fetch(
-        url,
-        {
-            method: 'GET',
-            headers: headers,
+        return {
+            status: resp.status,
+            data: resp.data
         }
-    );
-
-    if(await res.headers.get('Content-Type').includes('json')) {
-        response.body = await res.json();
-    } else {
-        response.body = await res.text();
     }
 
-    response.statusCode = res.status;
-    console.log(response.body);
-    return response;
 }
 
 export default new API
